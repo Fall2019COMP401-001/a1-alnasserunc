0151b095a316349741ce2e72428afc31f23a28bb
mohammedalnasser <mohammedalnasser@mohammeds-mbp.wireless-1x.unc.edu>
2019-08-27 11:34:38 -0400

    Editted the example program to match the autocorrector's expectations. Finsihed off the Novice program.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,48 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
+		// number of customers
+		// first name
+		// last name
+		// number of items
+		
+		// quantity
+		// name of item
+		// price
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int numberOfCustomers = scan.nextInt();
+		
+		String[] outputString = new String[numberOfCustomers];
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			
+			int numberOfItems = scan.nextInt();
+			
+			// input the items to be purchased
+			String[][] items = new String[3][numberOfItems];
+			for (int j = 0; j < numberOfItems; j++) {
+				items[0][j] = scan.next();
+				items[1][j] = scan.next();
+				items[2][j] = scan.next();
+			}
+			
+			// calculate the output and save it
+			double sum = 0.0;
+			for (int j = 0; j < numberOfItems; j++) {
+				// source: https://www.journaldev.com/18392/java-convert-string-to-double
+				sum += Double.parseDouble(items[0][j]) * Double.parseDouble(items[2][j]);
+			}
+			
+			outputString[i] = firstName.substring(0, 1) + ". " + lastName + ": " + String.format("%.2f", sum);
+		}
+		
+		// print the output to the console
+		for (int i = 0; i < numberOfCustomers; i++) {
+			System.out.println(outputString[i]);
+		}
 	}
 }
