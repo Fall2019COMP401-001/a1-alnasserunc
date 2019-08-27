07d6b4fadb9c9e5b9027695064055becf632814d
mohammedalnasser <mohammedalnasser@mohammeds-mbp.wireless-1x.unc.edu>
2019-08-27 13:24:03 -0400

    Takes in the store's input and saves it in a String array itemsPrices.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,18 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		
+		// store's input
+		int numberOfItems = scan.nextInt();
+		
+		// even: name
+		// odd: price 
+		String[] itemsPrices = new String[numberOfItems * 2];
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			itemsPrices[i * 2] = scan.next(); // name
+			itemsPrices[i * 2 + 1] = scan.next(); // price
+		}
 		
 	}
 }
