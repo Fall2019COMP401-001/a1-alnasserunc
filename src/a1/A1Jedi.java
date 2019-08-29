aeaa4ff8a272ab019b9cb108c35744dee27f9ed2
mohammedalnasser <mohammedalnasser@mohammeds-mbp.wireless-1x.unc.edu>
2019-08-29 13:36:11 -0400

    Finished the entire Jedi Program

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -7,8 +8,76 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		// store's input
+		int numberOfItems = scan.nextInt();
+		
+		// even: name
+		// odd: price 
+		String[] itemsPrices = new String[numberOfItems * 2];
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			itemsPrices[i * 2] = scan.next(); // name
+			itemsPrices[i * 2 + 1] = scan.next(); // price
+		}
+		
+		// customers' input
+		int numberOfCustomers = scan.nextInt();
+		Customer[] customers = new Customer[numberOfCustomers];
+		
+		for (int i = 0; i < customers.length; i++) {
+			// fill out the customers data
+			
+			Customer currentCustomer = new Customer();
+			customers[i] = currentCustomer;
+			
+			customers[i].setFirstName(scan.next());
+			customers[i].setLastName(scan.next());
+			customers[i].itemCount = scan.nextInt();
+			customers[i].itemsBought = new Item[customers[i].itemCount];
+			
+			for (int j = 0; j < customers[i].itemCount;  j++) {
+				// fill out each item's info
+				Item currentItem = new Item();
+				customers[i].itemsBought[j] = currentItem;
+				customers[i].itemsBought[j].quantity = scan.nextInt();
+				customers[i].itemsBought[j].name = scan.next();
+			}
+		}
+		
+		String[] itemNames = new String[numberOfItems];
+		int[] customerCount =  new int[numberOfItems];
+		int[] itemCount =  new int[numberOfItems];
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			itemNames[i] = itemsPrices[i * 2];
+			customerCount[i] = 0;
+			itemCount[i] = 0;
+		}
+		
+		for (int i = 0; i < customers.length; i++) {
+			for (int j = 0; j < customers[i].itemsBought.length; j++) {
+				customerCount[findIndex(itemNames, numberOfItems, customers[i].itemsBought[j].name)] += 1;
+				itemCount[findIndex(itemNames, numberOfItems, customers[i].itemsBought[j].name)] += customers[i].itemsBought[j].quantity;
+			}
+		}
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			if (customerCount[i] == 0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			} else {
+				System.out.println(customerCount[i] + " customers bought " + itemCount[i] + " " + itemNames[i]);
+			}
+		}
+		
+	}
+	
+	public static int findIndex (String[] arr, int len, String str) {
+		for (int i = 0; i < len; i++) {
+			if (arr[i].equals(str)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
