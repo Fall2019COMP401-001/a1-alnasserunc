0e012507baebc75f8f4ee78e942dfc74db8d5c69
mohammedalnasser <mohammedalnasser@mohammeds-mbp.wireless-1x.unc.edu>
2019-08-29 11:01:10 -0400

    Created Customer and Item classes

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -2,9 +2,29 @@ package a1;
 
 import java.util.Scanner;
 
+
+
 public class A1Adept {
+	public class Item {
+		String name;
+		double price;
+		int quantity;
+	}
+	
+	public class Customer {
+		String firstName, lastName;
+		int itemCount;
+		Item[] itemsBought;
+		
+		public Customer(int amountOfItems) {
+			itemsBought = new Item[amountOfItems];
+			itemCount = amountOfItems;
+		}
+	}
 
 	public static void main(String[] args) {
+		
+		
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -19,6 +39,16 @@ public class A1Adept {
 			itemsPrices[i * 2] = scan.next(); // name
 			itemsPrices[i * 2 + 1] = scan.next(); // price
 		}
+		
+		/* 
+		 * An integer count of the number of customers
+		 * Then for each customer...
+		 * The first name of the customer
+		 * The last name of the customer
+		   */
+		// customers' input
+		int numberOfCustomers = scan.nextInt();
+		
 		
 	}
 }
